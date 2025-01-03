ed4b314a0552f716e1d09e87780502648ba56cf2
Leo Chen <46356943+SSShee77y@users.noreply.github.com>
2025-01-02 21:51:01 -0500

    Maybe fixed cube.gif

--- 2025-01-replace-cube/replace.js
+++ 2025-01-replace-cube/replace.js
@@ -1,14 +1,10 @@
 const button = document.getElementById('replace-button');
-const bgImage = document.getElementById('bg-img');
+let bgImage = document.getElementById('bg-img');
 const gifSrc = 'cube.gif';
-
-// Preload the GIF
-const gifPreload = new Image();
-gifPreload.src = gifSrc;
 
 button.addEventListener('click', () => {
     button.disabled = true;
-    bgImage.src = `${gifSrc}?t=${Date.now()}`;
+    bgImage.src = `cube.gif?v=${Math.random()}`;
 
     setTimeout(() => {
         button.disabled = false;
